eed2c78722ff889c7227b11ead487ce9303ae223
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-12-27 18:17:49 -0300

    criacao de dockerfile

--- pedidosbtg/src/main/java/br/com/emanueldias01/pedidosbtg/PedidosbtgApplication.java
+++ pedidosbtg/src/main/java/br/com/emanueldias01/pedidosbtg/PedidosbtgApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PedidosbtgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PedidosbtgApplication.class, args);
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        SpringApplication.run(PedidosbtgApplication.class, args);
 	}
 
 }
