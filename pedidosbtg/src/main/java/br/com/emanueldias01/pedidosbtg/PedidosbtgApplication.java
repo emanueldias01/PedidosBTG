31e490aa5f693e41e1063578b2bd3655df5d2935
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-12-27 13:58:42 -0300

    add files

---
+++ pedidosbtg/src/main/java/br/com/emanueldias01/pedidosbtg/PedidosbtgApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias01.pedidosbtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosbtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosbtgApplication.class, args);
+	}
+
+}
---
+++ pedidosbtg/src/test/java/br/com/emanueldias01/pedidosbtg/PedidosbtgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias01.pedidosbtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosbtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
